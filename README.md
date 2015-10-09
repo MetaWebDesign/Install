47f6e771c82a19c6a18c3faef298fb89a22fb14f
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2015-10-09 16:10:30 -0300

    Update install

---
+++ README.md
@@ -0,0 +1,24 @@
+# INSTALL META WEB DESIGN
+
+##GNU LINUX (Ubuntu / Mint / Debian / Trisquel)
+
+1 - Download and unzip Metawebdesign installer.
+2 - Give execute permissions to the installer with the "chmod + x install.sh" command
+```bash
+chmod + x install.sh
+```
+3 - Run the "./install.sh" installer
+```bash
+./install.sh
+```
+4 - will be installed: git, apache, sqlite3, PHP5 a modified version of eclipse with the model editor based on OO-Method and a compiler for PHP code.
+5 - A ejecturable can I create in your "home" directory called "run_metawebdesign.sh"
+6 - To run you must give execute permissions with "chmod 777 run_metawebdesign.sh"
+```bash
+cd ~/
+chmod 777 run_metawebdesign.sh
+```
+7 - to run "./run_metawebdesign.sh"
+```bash
+./run_metawebdesign.sh
+```
--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #INSTALL METAWEBDESIGN
+clear
+echo -e "\n-------------------------------------------------------------------\n"
+echo -e "			META WEB DESING - INSTALL\n"
+echo -e " 		     https://github.com/MetaWebDesign\n"
+echo -e "-------------------------------------------------------------------\n"
+sleep 3
 sudo apt-get install sqlite3 apache2 php5 php5-sqlite git wget
 sudo mkdir /home/mwd
 sudo chmod 777 /home/mwd/
 sudo ln -s /home/mwd/runtime-EclipseApplication/ /var/www/html/mwd
 cd /home/mwd
+git clone https://github.com/MetaWebDesign/CompilerPHP.git
+git clone https://github.com/MetaWebDesign/Editor.git
 wget http://www.piratevalpo.cl/desarrollo/mwd.tar.gz
 tar -xvf mwd.tar.gz
+echo "cd  /home/mwd/MWD" > run.sh
+echo "./eclipse" >> run.sh
 ln -s /home/mwd/run.sh ~/run_metawebdesign.sh
 chmod +x ~/run_metawebdesign.sh
 echo -e "\n-------------------------------------------------------------------\n"
-echo -e "\nDone :) \n"
-echo -e "Metawebdesign you can run using the bash script in your home folder \"run_metawebdesign.sh\""
-echo -e "\n-------------------------------------------------------------------\n"
+echo -e "Done :) \n"
+echo -e "Metawebdesign you can run using the bash script in your home folder \"run_metawebdesign.sh\"\n"
+echo -e "-------------------------------------------------------------------\n"
